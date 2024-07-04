46975d9e30afd726e861366697a2f34d7db9e062
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-04 12:18:56 +0530

    added input feature and design to the app

--- food-recipe/src/components/navbar/index.jsx
+++ food-recipe/src/components/navbar/index.jsx
@@ -1,3 +1,15 @@
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between py-8 itemsgap-5 mx-auto-col lg:flex -row lg :gap-0">
+      <h2 className="text-2xl font-semibold">FoodRecipe</h2>
+      <form>
+        <input
+          type="text"
+          name="search"
+          placeholder="Enter items ...."
+          className="p-3 px-8 rounded-full shadow-lg outline-none lg:w-96 shadow-red-100 focus-shadow-red-200 bg-white/75"
+        />
+      </form>
+    </nav>
+  );
 }
